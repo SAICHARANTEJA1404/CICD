7f9e2802843db19d7a2827ec30d3c4687e38bfb1
SAICHARANTEJA1404 <143397186+SAICHARANTEJA1404@users.noreply.github.com>
2026-06-02 15:26:30 +0530

    Add CI PipeLine

---
+++ src/test/java/org/opencode/demo/controllers/HelloControllerTest.java
@@ -0,0 +1,18 @@
+package org.opencode.demo.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HelloControllerTest {
+
+    @Test
+    void testHello() {
+        HelloController controller = new HelloController();
+
+        assertEquals(
+                "Hello World",
+                controller.hello()
+        );
+    }
+}
